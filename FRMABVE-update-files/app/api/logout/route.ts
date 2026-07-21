692b5b3509f80d1018c8826bb787e5e8bda8612f
thelifeoferic <thelifeoferic@gmail.com>
2026-07-20 18:28:58 -0700

    Delete FRMABVE-update-files directory

--- FRMABVE-update-files/app/api/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { SESSION_COOKIE } from "@/lib/auth/lock";
-
-export async function POST(request: Request) {
-  const response = NextResponse.redirect(new URL("/asheville/login", request.url), 303);
-  response.cookies.delete(SESSION_COOKIE);
-
-  return response;
-}
--- FRMABVE-update-files/app/asheville/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CampaignStudio } from "@/components/CampaignBuilder/CampaignStudio";
-
-export default function Home() {
-  return <CampaignStudio />;
-}
