9c2d806a083445c0fbcb9897fe418c16b423e616
thelifeoferic <thelifeoferic@gmail.com>
2026-07-20 18:28:50 -0700

    Delete frmabve-clean-root-upload-20260720-v25 2 directory

--- frmabve-clean-root-upload-20260720-v25 2/app/api/audiences/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getAlpineAudienceResult } from "@/lib/integrations/alpine-iq";
-
-export async function GET() {
-  const result = await getAlpineAudienceResult();
-  return NextResponse.json(result);
-}
